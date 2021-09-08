cb14e4107b1e7ef8fffd06f515a4b1f4b30f09df
Jean Francis Dominique S. Mabalot <jfdsmabalot@gmail.com>
2021-09-09 01:34:00 +0800

    examples

--- python_basics/functions.py
+++ python_basics/functions.py
@@ -1,4 +1,23 @@
-# functions "def mean():""
+# functions "def mean():"
 
 def mean(mylist):
+    the_mean = sum(mylist) / len(mylist)
+    return the_mean
+
+print(mean([1, 4, 5]))
+print(type(mean), type(sum))
+
+def fluid_ml(oz):
+    return oz * 29.57353
     
+print(fluid_ml)
+
+def foo(three):
+    return three * three
+    
+print(foo)
+
+def mean(mylist):
+    print("Function start!")
+    the_mean = sum(mylst) / len(mylist)
+    return the_mean
